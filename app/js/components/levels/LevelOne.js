22c34656c5ca0e3f820d2d9c85b88fd9ee41b0e1
Raul Alvarez Gonzalez <raulalgo@gmail.com>
2017-11-03 15:39:57 +0000

    Levels cleaned up

--- app/js/components/levels/LevelOne.js
+++ app/js/components/levels/LevelOne.js
@@ -3,63 +3,28 @@
 'use strict';
 
 import React          from 'react';
-import Button         from '../Button';
-import RipplerButton  from '../RipplerButton';
-import LevelFrame     from '../LevelFrame';
+import styled         from 'styled-components';
 
 class LevelOne extends React.Component {
-  transition;
-  solved;
-  exitButton;
-  extra
 
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.resolve = this.resolve.bind(this);
-    this.nextLevel = this.nextLevel.bind(this);
-
-    this.state = {
-      orangeHidden  : true,
-      solved        : false,
-      extra         : ''
-    };
-    this.solved = false;
   }
   render() {
-    // if(this.state.solved){
-    //   this.exitButton = <RipplerButton color='orange' transition='fall' onClick={this.nextLevel} />
-    //   console.log('this.exitButton');
-    // } else {
-    //   this.exitButton = '
-    // }
     return (
-        <RipplerButton color='exit ' transition='fall ' onClick={this.nextLevel} lights='lightOn ' />
+        <div>Level</div>
     );
   }
 
   handleClick () {
-    console.log('click');
-    this.setState({
-      orangeHidden  : false
-    });
 
   }
 
   resolve() {
-    this.setState({
-      solved  : true,
-      extra   : 'deactivate'
-    });
-    console.log('resolve');
   }
 
   nextLevel() {
-    console.log('Next Level');
-    //onsole.log(this.props.message);
-    this.props.newLevel();
   }
 }
 
 export default LevelOne;
-//
