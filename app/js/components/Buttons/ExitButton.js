112c5a68fc4538b7cc26963356f3111e5fef1953
Raúl Álvarez González <raulalgo@obturecode.com>
2017-11-17 18:18:27 +0000

    Annotatios nor the future

--- app/js/components/Buttons/ExitButton.js
+++ app/js/components/Buttons/ExitButton.js
@@ -4,6 +4,15 @@ import React          from 'react';
 
 import RipplerButton  from  './RipplerButton'
 
+/**
+ * Turn this button into a TogglerButton + rippler.
+ * This will have three states that move:
+ * 
+ *     Grey  ->  White -->  Orange  ->  Ripple
+ * 
+ * The color that gets selected determines the steps needed to the final ripple
+ */
+
 class ExitButton extends React.Component {
   constructor(props) {
     super(props);
