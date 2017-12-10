720d0e3fb81e1ad2dc0052908b6d5831b445fb47
Raúl Álvarez González <raulalgo@obturecode.com>
2017-12-10 23:30:41 +0000

    playing around, but not working

--- app/js/components/Test/MotionTest.js
+++ app/js/components/Test/MotionTest.js
@@ -1,66 +1,69 @@
 import React, { Component } from 'react';
-import {Motion, spring} from 'react-motion';
+import {StaggeredMotion, spring, presets} from 'react-motion';
 import styled from 'styled-components';
 
-const Rojo = styled.div`
-    background: red;
-    width: 100px;
-    height: 100px;
-`;
-
-const Demo0 = styled.div`
-    border-radius: 4px;
-    background-color: rgb(240, 240, 232);
-    position: relative;
-    margin: 5px 3px 10px;
-    width: 450px;
-    height: 50px;
-`;
-
-const Demo0Block = styled.div`
+const Demo1 = styled.div`
+    width: 100%;
+    height: 100%;
     position: absolute;
-    width: 50px;
-    height: 50px;
-    border-radius: 4px;
-    background-color: rgb(130, 181, 198);
+`;
+const Pelota = styled.div`
+    background: red;
+    height: 100px;
+    width: 100px;
+    border-radius: 100%;
 `;
 
 class MotionTest extends Component {
     constructor(props) {
         super(props);
-        this.state = {open: false};
+        this.state = {x: 250, y:300};
     }
 
-    handleMouseDown() {
-        this.setState({open: !this.state.open});
+    componentDidMount() {
+        window.addEventListener('mousemove', this.handleMouseMove);
+        window.addEventListener('touchmove', this.handleTouchMove);
     }
 
-    handleTouchStart(e) {
-        e.preventDefault();
-        this.handleMouseDown();
-    }
+    handleMouseMove({pageX: x, pageY: y}) {
+        this.setState({x,y});
+    };
+
+    handleTouchMove({touches}) {
+        this.handleMouseMove(touches[0]);
+    };
+
+    getStyles(prevStyles) {
+        const endValue = prevStyles.map((_, i) => {
+            return i === 0
+                ? this.state
+                : {
+                    x: spring(prevStyles[i-1].x, presets.gentle),
+                    y: spring(prevStyles[i-1].y, presets.gentle),
+                };
+        });
+        return endValue;
+    };
 
     render() {
         return (
-        <div>
-            <button
-                onMouseDown={this.handleMouseDown}
-                onTouchStart={this.handleTouchStart} >
-                Toggle
-            </button>
-
-            <Motion style={{x: spring(this.state.open ? 400 : 0)}} >
-                {({x}) =>
-                    <Demo0 className='demo0' >
-                        <Demo0Block className='demo0-block' style={{
-                            WebkitTransform: `translate3d(${x}px, 0, 0)`,
-                            transform: `translate3d(${x}px, 0, 0)`,
-                        }} />
-                    <div>{x.value}</div>
-                    </Demo0>
+            <StaggeredMotion
+                styles={this.getStyles} >
+                {balls => 
+                    <Demo1>
+                    {balls.map(({x, y}, i) => 
+                        <Pelota
+                            style= {{
+                                WebkitTransform: `translate3d(${x-50}px, ${y-50}px, 0)`,
+                                transform: `translate3d(${x-50}px, ${y-50}px, 0`,
+                                zIndex: balls.length -i,
+                            }}
+                        />
+                    )}
+                    </Demo1>
                 }
-            </Motion>
-        </div>
+            </StaggeredMotion>
+
         );
     }
 }
